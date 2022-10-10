eb25b349a9ccb6ad73161f59aca0f893706ec990
vitormaco <vitorcmcorrea@gmail.com>
2022-10-10 11:43:39 +0200

    Add agglomeration method

--- clustering.py
+++ clustering.py
@@ -29,23 +29,39 @@ DataType = list[int, int]
 
 def find_cluster(
     data: DataType,
-    cluster_method: Callable[[DataType, int], DataType],
-    score_method: Callable[[DataType, int], int]
+    cluster_method,
+    score_method: Callable[[DataType, int], int],
+    mode,
+    params,
 ):
     initial_time = time.time()
     best_labels = (0, [0 for _ in data])
     best_score = None
     best_n_clusters = None
-    for n_clusters in range(2,round(len(data)**0.5)):
-        labels = cluster_method(data, n_clusters)
-        score = score_method(data, labels)
-        if not best_score or score > best_score:
-            best_score = score
-            best_n_clusters = n_clusters
-            best_labels = labels
-    final_time = time.time()
-    execution_time = final_time - initial_time
-    return best_labels, best_score, best_n_clusters, execution_time
+    best_params = None
+
+    if mode == "range":
+        for n_clusters in range(2,round(len(data)**0.5)):
+            labels = cluster_method(data, n_clusters)
+            score = score_method(data, labels)
+            if not best_score or score > best_score:
+                best_score = score
+                best_n_clusters = n_clusters
+                best_labels = labels
+        execution_time = time.time() - initial_time
+        return best_labels, best_score, best_n_clusters, execution_time, best_params
+
+    if mode == "agglomeration":
+        for p in params:
+            labels, n_clusters = cluster_method(data, p)
+            score = score_method(data, labels)
+            if not best_score or score > best_score:
+                best_score = score
+                best_n_clusters = n_clusters
+                best_labels = labels
+                best_params = p
+        execution_time = time.time() - initial_time
+        return best_labels, best_score, best_n_clusters, execution_time, best_params
 
 def find_kmeans(data, n_clusters):
     model = cluster.KMeans(n_clusters = n_clusters, init = 'k-means++')
@@ -57,16 +73,32 @@ def find_kmedoids(data, n_clusters):
     fp = kmedoids.fasterpam(distmatrix, n_clusters)
     return fp.labels
 
-# def find_agglomerative(data, n_clusters):
-#     model = cluster. AgglomerativeClustering(
-#         distance_threshold = 10,
-#         linkage = "single",
-#         n_clusters = None
-#     )
-#     model = model.fit(data)
-#     model.n_clusters_
-#     leaves = model.n_leaves_
-#     return model.labels_
+def find_agglomerative(data, params):
+    model = cluster. AgglomerativeClustering(
+        distance_threshold = params[1],
+        linkage = params[0],
+        n_clusters = None
+    )
+    model = model.fit(data)
+    return model.labels_, model.n_clusters_
+
+CLUSTER_METHODS = {
+    "kmeans" : {
+        "method": find_kmeans,
+        "mode": "range",
+        "params" : []
+    },
+    "kmetoids": {
+        "method": find_kmedoids,
+        "mode": "range",
+        "params" : []
+    },
+    "agglomerative": {
+        "method": find_agglomerative,
+        "mode": "agglomeration",
+        "params" : [[linkage, distance] for linkage in ["single", "ward", "complete", "average"] for distance in [5, 10, 20]]
+    },
+}
 
 SCORE_METHODS = [
     ("SILHOUETTE", SILHOUETTE),
@@ -74,19 +106,15 @@ SCORE_METHODS = [
     ("CALINSKI", CALINSKI),
 ]
 
-CLUSTER_METHODS = [
-    ("KMEANS", find_kmeans),
-    ("KMEDOIDS", find_kmedoids),
-]
 
 with open("./results.csv", 'w') as out:
     csv_out = csv.writer(out)
-    csv_out.writerow(("method", "score_method", "score", "ideal n clusters", "execution time"))
-    for cluster_method_name, cluster_method in CLUSTER_METHODS:
+    csv_out.writerow(("method", "score_method", "score", "ideal n clusters", "execution time", "best params"))
+    for cluster_method_name, cluster_method in CLUSTER_METHODS.items():
         for score_method_name, score_method in SCORE_METHODS:
-            print("running " + str(cluster_method_name) + " with score " + str(score_method_name))
-            labels, score, n_cluster, execution_time = find_cluster(data, find_kmeans, score_method)
-            csv_out.writerow((cluster_method_name, score_method_name, score, n_cluster, execution_time))
+            print("running " + str(cluster_method_name) + " with score " + str(score_method_name) + " with params " + str(cluster_method["params"]))
+            labels, score, n_cluster, execution_time, best_params = find_cluster(data, cluster_method["method"], score_method, cluster_method["mode"], cluster_method["params"])
+            csv_out.writerow((cluster_method_name, score_method_name, score, n_cluster, execution_time, best_params))
 
 # plt.scatter(f0, f1, c=labels, s=3)
 # plt.show()
